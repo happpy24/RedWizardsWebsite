d4016304bc787dd75afe32c74e92d8b5e3b4face
V. Speijer <110664@st-maartenscollege.nl>
2022-03-18 08:54:13 +0100

    Add project files.

---
+++ RedWizards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedWizards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
